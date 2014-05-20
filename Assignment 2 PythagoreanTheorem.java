9451feb404e801120796dbe4da1f948b8f30837b
luobopi <xingyi.ouc@gmail.com>
2014-05-20 18:53:18 -0400

    Create Assignment 2 PythagoreanTheorem.java

---
+++ Assignment 2 PythagoreanTheorem.java
@@ -0,0 +1,20 @@
+/*
+ * File: PythagoreanTheorem.java
+ * Name: 
+ * Section Leader: 
+ * -----------------------------
+ * This file is the starter file for the PythagoreanTheorem problem.
+ */
+
+import acm.program.*;
+//import java.lang.Math;
+
+public class PythagoreanTheorem extends ConsoleProgram {
+	public void run() {
+		println("Enter values to compute Pythagorean theoren.");
+		int a = readInt("a: ");
+		int b = readInt("b: ");
+		double c=Math.sqrt(a*a+b*b);
+		println("c = "+c);
+	}
+}
