71eb808ff506283022328cbe868946df7a264290
luobopi <xingyi.ouc@gmail.com>
2014-06-06 16:35:56 -0400

    Create Assignment 6 NameSurfer_milestone3.java

---
+++ Assignment 6 NameSurfer_milestone3.java
@@ -0,0 +1,53 @@
+/*
+ * File: NameSurfer.java
+ * ---------------------
+ * When it is finished, this program will implements the viewer for
+ * the baby-name database described in the assignment handout.
+ */
+
+import acm.program.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+//public class NameSurfer extends Program implements NameSurferConstants {
+public class NameSurfer extends ConsoleProgram{
+/* Method: init() */
+/**
+ * This method has the responsibility for reading in the data base
+ * and initializing the interactors at the bottom of the window.
+ */
+	public void init() {
+	   add(new JLabel("Name: "),SOUTH);
+	   nameField=new JTextField(20);
+	   add(nameField,SOUTH);
+	   nameField.addActionListener(this);
+	   //nameField.addActionListener(this);
+	   graphButton=new JButton("Graph");
+	   add(graphButton,SOUTH);
+	   clearButton=new JButton("Clear");
+	   add(clearButton,SOUTH);
+	   
+	   addActionListeners();
+		// You fill this in, along with any helper methods //
+	}
+
+/* Method: actionPerformed(e) */
+/**
+ * This class is responsible for detecting when the buttons are
+ * clicked, so you will have to define a method to respond to
+ * button actions.
+ */
+	public void actionPerformed(ActionEvent e) {
+		NameSurferEntry nameranking=namedb.findEntry(nameField.getText());
+		if(e.getSource()==graphButton || e.getSource()==nameField) {
+			println("Graphic: "+nameranking);
+		}else if(e.getSource()==clearButton) {
+			println("Clear");
+		}
+	}
+	/* Private instance varibales */
+	private JTextField nameField;
+	private JButton graphButton;
+	private JButton clearButton;
+	private NameSurferDataBase namedb=new NameSurferDataBase("names-data.txt");
+}
